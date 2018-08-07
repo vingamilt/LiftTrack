a27dae9e171a8c7bd1100fc06753f135778dc026
vingamilt <tglanton@gamil.com>
2018-08-07 00:21:05 -0400

    Updated basic pages for foundation

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,7 +3,9 @@ var current_day = d.getDay()
 var el = document.getElementsByClassName("workout_day");
 var activity_day = ["Last Completed Day","Day 1","Day 2","Day 3","Day 4","Day 5","Day 6","Day 7"];
 var activity_day_selector = document.getElementById("activity_day_selector");
+var completeDay = document.getElementById("complete_day");
 
+completeDay.addEventListener("click",updateCompleteDayMessage);
 // Update the class elements indicating the current workout day. 
 // This will update to the day of the week, NOTE: may need adjustment based on usage.
 function updateWorkoutDays () {
@@ -20,6 +22,12 @@ function updateActivityDaySelector() {
         activity_day_selector.appendChild(opt);
     });
 }
+
+function updateCompleteDayMessage() {
+    document.getElementById("message").innerHTML = "Nice Job!!";
+}
+
+
 
 updateWorkoutDays();
 updateActivityDaySelector();
